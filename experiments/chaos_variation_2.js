0d6c301bbb1e7db86c747400cf6d9849112aaa69
Melvin Kjellén <melvin.kjellen@epitech.eu>
2025-10-02 13:21:24 +0200

    Finished variation 2

--- experiments/chaos_variation_2.js
+++ experiments/chaos_variation_2.js
@@ -28,3 +28,14 @@ function setup(){
         }
     }
 }
+
+function polygon(x, y, radius, npoints){
+    let angle = TWO_PI / npoints;
+    beginshape();
+    for (let a = 0; a < TWO_PI; a += angle) {
+        let sx = x + cos(a) * radius;
+        let sy = y + sin(a) * radius;
+        vertex(sx, sy);
+    }
+    endShape(CLOSE);
+}
